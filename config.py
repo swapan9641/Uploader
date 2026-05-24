8f91ca3183cfb7c95def23403c66bc2712c1d84c
swapan9641 <xyz686@atomicmail.io>
2026-05-24 16:31:37 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,12 +3,12 @@ import os
 ##Code Written By @ItsMeMaster
 
 class Config(object):
-    BOT_TOKEN = "8525803667:AAEjpylk_qaZ62O_gIZmch46S8VQNYxcY1M"
-    DB_NAME = "nattu"
-    API_ID = "6886135"
-    API_HASH = "ee20a1c8a8e44eaa638b7254cbcc3012"
-    ADMIN_ID = 2118600611
-    DB_URL = "mongodb+srv://nattu:nattu@cluster0.quvds.mongodb.net/?appName=Cluster0"
-    LOG_CHANNEL = "-1003744110162" # Your Log Channel ID (Bot ko ADMIN BNAYE)
-    USERLINK = "https://t.me/Natking_uploader_bot"
-    TUTORIAL_VIDEO = "https://t.me/Natking_uploader_bot"
+    BOT_TOKEN = ""
+    DB_NAME = "batch"
+    API_ID = ""
+    API_HASH = ""
+    ADMIN_ID = 
+    DB_URL = ""
+    LOG_CHANNEL = "" # Your Log Channel ID (Bot ko ADMIN BNAYE)
+    USERLINK = "https://t.me/dhuruv"
+    TUTORIAL_VIDEO = "@bots_zone"
